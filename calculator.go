5ac745d49c08ef85b43f55b7ef927ef391469037
skumarapperuma <skumarapperuma@innodata.com>
2026-01-06 14:17:41 +0530

    test

--- calculator.go
+++ calculator.go
@@ -15,3 +15,9 @@ func Multiply(x, y int) (int, error) {
 	return x * y, nil
 }
 
+func Divide(x, y int) (float64, error) {
+	if y == 0 {
+		return 0, errors.New("Cannot divide by 0")
+	}
+	return float64(x) / float64(y), nil
+}
